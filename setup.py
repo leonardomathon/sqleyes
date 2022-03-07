3a98908fd98f91d5146b3ac612d9d336780bc1e1
leonardomathon <leonardo.mathon@hotmail.com>
2022-03-07 16:46:53 +0100

    Change README from restructured text to markdown

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read_file(file):
    with open(file) as f:
         return f.read()
     
-long_description = read_file("README.rst")
+long_description = read_file("README.md")
 version = read_file("VERSION")
 requirements = read_requirements("requirements.txt")
 
@@ -19,7 +19,7 @@ setup(
     author_email = 'info@leonardomathon.nl',
     url = 'leonardomathon.nl',
     description = 'A CLI in Python that analyzes raw SQL queries for common anti-patterns',
-    long_description_content_type = "text/x-rst",  # If this causes a warning, upgrade your setuptools package
+    long_description_content_type = "text/markdown",  # If this causes a warning, upgrade your setuptools package
     long_description = long_description,
     license = "MIT license",
     packages = find_packages(exclude=["test"]),  # Don't include test directory in binary distribution
