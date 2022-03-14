93dccd2a330f3de4af17bf4ee5dfd9c686b8d915
leonardomathon <leonardo.mathon@hotmail.com>
2022-03-14 20:35:48 +0100

    Add certainty to detector output

--- sqleyes/detector/detector_output.py
+++ sqleyes/detector/detector_output.py
@@ -7,17 +7,21 @@ class DetectorOutput:
     This class represents the output of a detector.
 
     Attributes:
+        certainty (int): The certainty of the detector detecting the AP.
         detector_type (str): The type of detector that produced this output.
         type (str): The type of output.
     """
-    def __init__(self, detector_type: str, type: str):
+    def __init__(self, certainty: int, detector_type: str, type: str):
+        # Clamp certainty between 0 and 1
+        self.certainty = sorted([0, certainty, 100])[1]
         self.detector_type = detector_type
         self.type = type
 
     def __create_dictionary(self):
         return {
-            "type": self.type,
-            "detector_type": self.detector_type
+            "certainty": self.certainty,
+            "detector_type": self.detector_type,
+            "type": self.type
         }
 
     def __repr__(self):
