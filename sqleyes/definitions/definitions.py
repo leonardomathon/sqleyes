eed82c2ff5d5e33e129e0aba7935a9651e958d12
leonardomathon <leonardo.mathon@hotmail.com>
2022-03-15 21:24:58 +0100

    Linting

--- sqleyes/definitions/definitions.py
+++ sqleyes/definitions/definitions.py
@@ -2,23 +2,18 @@ DEFINITIONS = {
     "anti_patterns": {
         "ambiguous_groups": {
             "type": "Ambiguous Groups",
-            "raw_type": "ambiguous_groups"
         },
         "fear_of_the_unknown": {
             "type": "Fear of the Unknown",
-            "raw_type": "fear_of_the_unknown"
         },
         "implicit_columns": {
             "type": "Implicit Columns",
-            "raw_type": "implicit_colums"
         },
         "poor_mans_search_engine": {
             "type": "Poor Man's Search Engine",
-            "raw_type": "poor_mans_search_engine"
         },
         "random_selection": {
             "type": "Random Selection",
-            "raw_type": "random_selection"
         }
     }
 }
